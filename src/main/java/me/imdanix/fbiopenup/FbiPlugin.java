e580208a752a64868d12d0faf8febc8668e5b3f4
Daniil Z <idanix@list.ru>
2023-05-12 17:08:53 +0300

    Initial commit

---
+++ src/main/java/me/imdanix/fbiopenup/FbiPlugin.java
@@ -0,0 +1,17 @@
+package me.imdanix.fbiopenup;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class FBIOpenUp extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
